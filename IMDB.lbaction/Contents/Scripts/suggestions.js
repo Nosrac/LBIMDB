9981c3e297d907e97e5f27d0f7b3a59d8309a27b
Kyle Carson <elyknosrac@gmail.com>
2014-07-15 19:23:02 -0500

    More informative results if nothing is returned

--- IMDB.lbaction/Contents/Scripts/suggestions.js
+++ IMDB.lbaction/Contents/Scripts/suggestions.js
@@ -1,47 +1,61 @@
+function sleep(milliseconds) {
+	var end = new Date().getTime() + milliseconds;
+	while (end > new Date().getTime())
+	{
+		continue;
+	}
+}
+
 function runWithString(argument) {
 
-    var result = HTTP.get("http://www.omdbapi.com/?i=&s=" + encodeURIComponent(argument), 3),
-        results = JSON.parse(result.data);
+	sleep(300); // Slow our requests, script will be halted if the argument changes
 
-   results = results.Search;
+	var result = HTTP.get("http://www.omdbapi.com/?i=&s=" + encodeURIComponent(argument), 3),
+		results = JSON.parse(result.data);
 
-   File.writeJSON(results, "/tmp/blah.txt");
+	if (result === undefined) {
+		return [{
+			title: "Error!",
+			icon: "at.obdev.LaunchBar:InfoTemplate",
+		}];
+	}
+	if ("Error" in results) {
+		var error = "No results for " + argument;
+		return [{
+			title: error,
+			subtitle: results.Error,
+			icon: "at.obdev.LaunchBar:InfoTemplate",
+		}];
+	}
 
-    if (result === undefined) {
-        LaunchBar.log('HTTP.getJSON() returned undefined');
-        return [];
-    }
-    if (result.error !== undefined) {
-        LaunchBar.log('Error in HTTP request: ' + result.error);
-        return [];
-    }
+	results = results.Search;
 
-    var suggestions = [];
-    var addedSuggestions = {};
+	var suggestions = [];
+	var addedSuggestions = {};
 
-    for (var i = 0; i < results.length; i++) {
-        var result = results[i];
-        var url = "http://www.imdb.com/title/" + result.imdbID;
+	for (var i = 0; i < results.length; i++) {
+		var result = results[i];
+		var url = "http://www.imdb.com/title/" + result.imdbID;
 
-        if (result.imdbID in addedSuggestions) continue;
+		if (result.imdbID in addedSuggestions) continue;
 
-        addedSuggestions[result.imdbID] = true;
+		addedSuggestions[result.imdbID] = true;
 
-        var icon = "at.obdev.LaunchBar:ActionTemplate";
+		var icon = "at.obdev.LaunchBar:ActionTemplate";
 
-        if (result.Type == "movie")
-        {
-            icon = "at.obdev.LaunchBar:MoviesTemplate";
-        } else if (result.Type == "series") {
-            icon = "at.obdev.LaunchBar:TVShowsTemplate";
-        }
+		if (result.Type == "movie")
+		{
+			icon = "at.obdev.LaunchBar:MoviesTemplate";
+		} else if (result.Type == "series") {
+			icon = "at.obdev.LaunchBar:TVShowsTemplate";
+		}
 
-        suggestions.push({
-                    title: result.Title,
-                    url: url,
-                    icon: icon,
-                });
-    }
+		suggestions.push({
+					title: result.Title,
+					url: url,
+					icon: icon,
+				});
+	}
 
-    return suggestions;
+	return suggestions;
 }
